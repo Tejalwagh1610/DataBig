e147409c155c369d566341e78e3695d7798932d4
Tejalwagh1610 <69747042+Tejalwagh1610@users.noreply.github.com>
2022-11-27 00:04:03 +0530

    Create Python_Operators.py

---
+++ Python_Operators.py
@@ -0,0 +1,19 @@
+# Numerical operators in Python
+
+# + for Addition
+# - for Subtraction
+# * for multiplication
+# / for Division
+# // for Integer division
+# ** for power calculations
+# % for Modulus
+
+x = 5
+y = 3
+print("Addition of x,y = ", x+y)
+print("Subtraction of x,y = ", x-y)
+print("Multiplition of x,y = ", x*y)
+print("Divion of x,y = ", x/y)
+print("Integer Division of x,y = ", x//y)
+print("Modulus of x,y = ", x%y)
+print("x to the power y = ", x**y)
