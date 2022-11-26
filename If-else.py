3f31535f3a15293e48e4759ca3adf80ae5b40e33
Tejalwagh1610 <69747042+Tejalwagh1610@users.noreply.github.com>
2022-11-27 02:35:39 +0530

    Update If-else.py

--- If-else.py
+++ If-else.py
@@ -9,4 +9,13 @@ else:
     print("No, x is not Equals to y!!")
 
 # is it mandetory to use the else block with if block?
-# 
+
+# a = 50
+# if a==50:
+#     print("Yes, a is equals to 50!!")
+# print("Bye!!")
+
+a = 40
+if a==50:
+    print("Yes, a is equals to 50!!")
+print("Bye!!")
