059677ac014cf61aec837d2abb878edb0175cdcb
Tejalwagh1610 <69747042+Tejalwagh1610@users.noreply.github.com>
2022-11-27 01:16:46 +0530

    Update Python_Operators.py

--- Python_Operators.py
+++ Python_Operators.py
@@ -17,3 +17,13 @@ print("Divion of x,y = ", x/y)
 print("Integer Division of x,y = ", x//y)
 print("Modulus of x,y = ", x%y)
 print("x to the power y = ", x**y)
+
+# Some operations of strings
+str_data = "Tejal"
+empty_str = ' '
+
+#Concat operation for strings
+# The '+' symbol acts as a concatanation operatorin python
+# If we use '-' symbol then error (unsupported) will appear As it is not defined
+full_name = str_data + " " + "Wagh"
+print("Full Name: ", full_name)
